50163b83d28749d1b8dbd5f01c9274cdf836f44d
Richard Zheng <Richard@Richards-MacBook-Pro-5.local>
2019-02-28 01:12:05 -0500

    score

--- tetris/tetris/GameScene.m
+++ tetris/tetris/GameScene.m
@@ -11,19 +11,25 @@
 @implementation GameScene {
     SKShapeNode *_spinnyNode;
     SKLabelNode *_label;
+    SKSpriteNode *boop;
+    int counter;
 }
 
 - (void)didMoveToView:(SKView *)view {
     // Setup your scene here
     
     // Get label node from scene and store it for use later
-    _label = (SKLabelNode *)[self childNodeWithName:@"//helloLabel"];
+    _label = (SKLabelNode *)[self childNodeWithName:@"Count"];
     
     _label.alpha = 0.0;
     [_label runAction:[SKAction fadeInWithDuration:2.0]];
-    
+    [_label setText:[NSString stringWithFormat: @"Score"]];
+    counter = 0;
     CGFloat w = (self.size.width + self.size.height) * 0.05;
-    
+  
+    [boop initWithImageNamed:@"maddenMillion.png"];
+  
+  
     // Create shape node to use during mouse interaction
     _spinnyNode = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(w, w) cornerRadius:w * 0.3];
     _spinnyNode.lineWidth = 2.5;
@@ -34,6 +40,9 @@
                                                 [SKAction fadeOutWithDuration:0.5],
                                                 [SKAction removeFromParent],
                                                 ]]];
+    UISwipeGestureRecognizer *up = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipe:)];
+    up.direction = UISwipeGestureRecognizerDirectionUp;
+    [self.view addGestureRecognizer:up];
 }
 
 
@@ -58,12 +67,35 @@
     [self addChild:n];
 }
 
+-(void)score {
+  [_label setText:[NSString stringWithFormat: @"%d", counter]];
+  if(counter < 5) counter++;
+  else if(counter >= 5 && counter < 10) counter = counter + 5;
+  else if(counter >= 10 && counter < 20) counter = counter + 10;
+  else if(counter >= 20 && counter < 50) counter = counter + 20;
+  else if(counter >= 50 && counter < 100) counter = counter + 50;
+  else if(counter >= 100 && counter < 500) counter = counter + 100;
+  else if(counter >= 500 && counter < 1000) counter = counter + 500;
+  else if(counter >= 1000 && counter < 10000) counter = counter + 1000;
+  else if(counter >= 10000 && counter < 100000) counter = counter + 10000;
+  else if(counter >= 100000 && counter < 1000000) counter = counter + 100000;
+  else counter = counter + 99999999;
+}
+
+-(void)swipe:(UISwipeGestureRecognizer *) sender {
+  if(sender.direction == UISwipeGestureRecognizerDirectionUp) {
+    //
+  }
+}
+
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     // Run 'Pulse' action from 'Actions.sks'
     [_label runAction:[SKAction actionNamed:@"Pulse"] withKey:@"fadeInOut"];
-    
+    [self score];
     for (UITouch *t in touches) {[self touchDownAtPoint:[t locationInNode:self]];}
+  
 }
+
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
     for (UITouch *t in touches) {[self touchMovedToPoint:[t locationInNode:self]];}
 }
